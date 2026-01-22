eba8c7352b131f86384c7154fc96a6a314c2b8d7
Aubin Joanes Igiraneza <aubinigiraneza1@gmail.com>
2026-01-22 20:37:17 +0200

    Task 0: inline styles in alx-react-app-new components

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer style={{ backgroundColor: "#333", color: "white", textAlign: "center" }}>
+      <p>Footer section</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+  return (
+    <main style={{ padding: "10px" }}>
+      <p>Main content goes here.</p>
+    </main>
+  );
+}
+
+export default MainContent;
