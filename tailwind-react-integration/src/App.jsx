2b8b79505eb6efa9a473558ecd64adc505d777ce
Aubin Joanes Igiraneza <aubinigiraneza1@gmail.com>
2026-02-04 10:21:04 +0200

    Add Tailwind React integration project

---
+++ tailwind-react-integration/src/App.jsx
@@ -0,0 +1,9 @@
+import UserProfile from "./components/UserProfile";
+
+export default function App() {
+  return (
+    <main className="min-h-screen bg-white flex items-center justify-center">
+      <UserProfile />
+    </main>
+  );
+}
