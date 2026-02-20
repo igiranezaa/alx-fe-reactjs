5d1bd54f22d888ed7a397c19867ab9338a8a3851
Aubin Joanes Igiraneza <aubinigiraneza1@gmail.com>
2026-02-20 16:57:58 +0200

    Fix file structure for TodoList component

---
+++ react-todo/src/components/TodoList.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+
+function TodoList() {
+  const [input, setInput] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = () => {
+    if (input.trim() === "") return;
+
+    setTodos([...todos, input]);
+    setInput("");
+  };
+
+  const deleteTodo = (index) => {
+    const newTodos = todos.filter((_, i) => i !== index);
+    setTodos(newTodos);
+  };
+
+  return (
+    <div>
+      <h1>Todo List</h1>
+
+      <input
+        type="text"
+        placeholder="Enter a task"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+
+      <button onClick={addTodo}>Add</button>
+
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo}
+            <button onClick={() => deleteTodo(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default TodoList;
